34fd865d535c9825e1328e23e868f45661f125a5
hmdne <54514036+hmdne@users.noreply.github.com>
2026-09-21 10:06:13 +0200

    refactor(js): remove obsolete Meta social layer

--- websieve/systems/social/content_type.js
+++ websieve/systems/social/content_type.js
@@ -26,6 +26,16 @@
     content.score = null;
   }
 
+  function socialCompactCount(n, stripCommas) {
+    var text = String(n);
+    if (stripCommas) text = text.replace(/,/g, "");
+    var num = parseInt(text, 10);
+    if (isNaN(num)) return n;
+    if (num >= 1000000) return (num / 1000000).toFixed(1).replace(/\.0$/, "") + "M";
+    if (num >= 10000) return (num / 1000).toFixed(1).replace(/\.0$/, "") + "K";
+    return num.toLocaleString();
+  }
+
   function socialPlatformLabel(metadata) {
     var siteName = normalizedSocialText(metadata && metadata.siteName);
     if (siteName && /^[\x00-\x7f]+$/.test(siteName) && !/^[\w.-]+\.[a-z]{2,}$/i.test(siteName)) {
